5f11e9b0090c908711e29901a1a881a22d81acc3
Tamas <fekete331@gmail.com>
2024-08-29 14:36:15 +0200

    All tests enabled and OK

--- tests/causalDiscoveryTest.cpp
+++ tests/causalDiscoveryTest.cpp
@@ -1,14 +1,8 @@
 #include <gtest/gtest.h>
 #include "graph.h"
 #include "dataset.h"
-#include "dataLoader.h"
 #include "causalDiscovery.h"
-#include <stdexcept>
-#include <memory>
-#include <fstream>
-#include <sstream>
-#include <vector>
-#include <string>
+#include "CSVReader.h"
 
 class CausalDiscoveryTest : public ::testing::Test
 {
@@ -36,67 +30,27 @@ TEST_F(CausalDiscoveryTest, UseVehicleDataSet)
     Columns:
     hengerûrtartalom	teljesítmény	Elhaladási zaj [dB(A)]	CO2 kibocsátás [g/km] (V.7.)
     */
-    std::string filename = "KV-41762_202301_test.csv";
-    std::ifstream file(filename);
-    ASSERT_TRUE(file.is_open()) << "Could not open the file!";
 
-    std::string line;
-    std::vector<Column> columns(4);  // Adjusted for 4 columns
-
-    while (std::getline(file, line)) {
-        std::stringstream ss(line);
-        std::string token;
-        std::vector<double> row_values;
-        bool valid_row = true;
-
-        for (int i = 0; i < 4; ++i) {
-            if (std::getline(ss, token, ',')) {
-                try {
-                    double value = std::stod(token);
-                    row_values.push_back(value);
-                }
-                catch (const std::invalid_argument&) {
-                    valid_row = false;
-                    break;
-                }
-                catch (const std::out_of_range&) {
-                    valid_row = false;
-                    break;
-                }
-            }
-            else {
-                valid_row = false;
-                break;
-            }
-        }
-
-        if (valid_row && row_values.size() == 4) {
-            for (int i = 0; i < 4; ++i) {
-                columns[i].push_back(row_values[i]);
-            }
-        }
-    }
-
-    file.close();
-
+    auto columns = CSVReader::readCSVFile("KV-41762_202301_test.csv", 4);
     auto data = std::make_shared<Dataset>(std::move(columns));
-
     auto graph = std::make_shared<Graph>(data);
 
     double alpha = 0.05;
-
     CausalDiscovery fci;
     fci.runFCI(graph, alpha);
     graph->printGraph();
 
     auto expectedGraph = std::make_shared<Graph>(data);
-    // expectedGraph->addDirectedEdge(...);
+    expectedGraph->addDirectedEdge(0,1);
+    expectedGraph->addDirectedEdge(0,2);
+    expectedGraph->addDirectedEdge(0,3);
+    expectedGraph->addDirectedEdge(1,3);
+    expectedGraph->addDirectedEdge(2,3);
 
-    //ASSERT_EQ(graph, expectedGraph) << "FCI algorithm should produce the expected graph structure";
+    ASSERT_EQ(graph, expectedGraph) << "FCI algorithm should produce the expected graph structure";
 }
 
-// Note: temporally disabled, because we are working on another implementatiopn
-TEST_F(CausalDiscoveryTest, DISABLED_SmokeTestFCIWithLargerDataset1)
+TEST_F(CausalDiscoveryTest, SmokeTestFCIWithLargerDataset1)
 {
     auto data = std::make_shared<Dataset>(std::vector<Column>{
         {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20},
