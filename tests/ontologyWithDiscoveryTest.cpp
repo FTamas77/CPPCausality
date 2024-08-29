d41f4d3600fd8c9e9b33aae62ec93159bc734d4b
Tamas <fekete331@gmail.com>
2024-08-29 12:46:06 +0200

    Apply Causal Discovery on real data

---
+++ tests/ontologyWithDiscoveryTest.cpp
@@ -0,0 +1,25 @@
+#include "Ontology.h"
+#include "Individual.h"
+#include "ClassType.h"
+#include "Property.h"
+#include <gtest/gtest.h>
+#include <iostream>
+#include <memory>
+#include <vector>
+
+class OntologyWithDiscoveryTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+    }
+
+    void TearDown() override {
+    }
+};
+
+TEST_F(OntologyWithDiscoveryTest, ReadInfoFromOntology) {
+    // Create an ontology
+    // Create data for causal discovery
+    // Read forbidden edges
+
+}
+
